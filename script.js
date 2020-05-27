9dd2ed779ca5e6f7ddce40fd5c76587c8bca0c74
jrolo024 <54976521+jrolo024@users.noreply.github.com>
2020-05-27 22:48:52 +0200

    Delete script.js

--- script.js
+++
@@ -1,2 +0,0 @@
-let button = document.getElementsByTagName("button")[0];
-button.onclick = function(){button.style.backgroundColor = `rgb(${Math.random()*255}, ${Math.random()*255}, ${Math.random()*255})`};
