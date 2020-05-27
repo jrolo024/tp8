11f11478233bc453924a8212ff9f7b8d90b918e6
jrolo024 <54976521+jrolo024@users.noreply.github.com>
2020-05-27 22:52:47 +0200

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-let button = document.getElementsByTagName("button")[0];
-document.onmousemove = function(event){button.innerText = `mouseX : ${event.clientX}
-mouseY : ${event.clientY}`};
-button.onmouseover = function(){button.style.backgroundColor = `rgb(${Math.random()*255}, ${Math.random()*255}, ${Math.random()*255})`};
